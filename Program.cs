125c7ff0550e02e43d0daa9ca58f783a01dc96fe
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-02-27 15:11:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Meeting}/{action=Index}/{id?}"
+);
+
+app.Run();
