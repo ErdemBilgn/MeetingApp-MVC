41a7aa947ddf4788d718a141fb4b2b729c02a5d4
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-03 09:48:22 +0300

    Added Layout, Bootstrap, Libman

--- Program.cs
+++ Program.cs
@@ -4,11 +4,15 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
+app.UseRouting();
+
 // app.MapDefaultControllerRoute();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Meeting}/{action=Index}/{id?}"
+    pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 app.Run();
---
+++ README.md
@@ -0,0 +1,1 @@
+A Meeting App Created With ASP.NET 8.0 MVC
